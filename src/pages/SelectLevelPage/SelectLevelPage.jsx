9001a1334bce6fa60614ae0af2bb7886f3141a3f
Daria Magomedova <mielilon@gmail.com>
2023-09-12 22:49:36 +0300

    Добавляет новый режим игры

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -1,28 +1,36 @@
 import { Link } from "react-router-dom";
 import styles from "./SelectLevelPage.module.css";
+import { Checkbox } from "../../components/Checkbox/Checkbox";
+import { useState } from "react";
+
+const levels = [3, 6, 9];
 
 export function SelectLevelPage() {
+  const [isEasyMode, setIsEasyMode] = useState(false);
+
+  const easyPath = isEasyMode ? "/easy-mode" : "";
+
   return (
     <div className={styles.container}>
       <div className={styles.modal}>
         <h1 className={styles.title}>Выбери сложность</h1>
         <ul className={styles.levels}>
-          <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/3">
-              1
-            </Link>
-          </li>
-          <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/6">
-              2
-            </Link>
-          </li>
-          <li className={styles.level}>
-            <Link className={styles.levelLink} to="/game/9">
-              3
-            </Link>
-          </li>
+          {levels.map((level, index) => (
+            <li className={styles.level} key={level}>
+              <Link className={styles.levelLink} to={`/game/${level}${easyPath}`}>
+                {index + 1}
+              </Link>
+            </li>
+          ))}
         </ul>
+        <Checkbox
+          id={"easy-mode"}
+          name={"easy-mode"}
+          label={"Легкий режим (3 жизни)"}
+          onClick={() => {
+            setIsEasyMode(prev => !prev);
+          }}
+        ></Checkbox>
       </div>
     </div>
   );
