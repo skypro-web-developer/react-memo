d2380978b9b738b143da404e1cc03fa408693078
Denis <den.sautner@mail.ru>
2024-01-09 18:15:57 +0600

    added error counter component

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,13 +1,17 @@
 import { useParams } from "react-router-dom";
 
 import { Cards } from "../../components/Cards/Cards";
+import { ErrorCounter } from "../../components/ErrorCounter/ErrorCounter";
+import { useSelector } from "react-redux";
 
 export function GamePage() {
   const { pairsCount } = useParams();
+  const isActiveGameMode = useSelector(state => state.game.isActiveGameMode);
 
   return (
     <>
       <Cards pairsCount={parseInt(pairsCount, 10)} previewSeconds={5}></Cards>
+      {isActiveGameMode && <ErrorCounter />}
     </>
   );
 }
