650fe397c14beeec133f81ca7c1b9a038f583fd2
Lana <lolhowko@gmail.com>
2024-01-23 17:50:54 +0300

    add again all

--- src/pages/GamePage/GamePage.jsx
+++ src/pages/GamePage/GamePage.jsx
@@ -1,13 +1,18 @@
 import { useParams } from "react-router-dom";
 
 import { Cards } from "../../components/Cards/Cards";
+import { useSelector } from "react-redux";
+import { ErrorCounter } from "../../components/ErrorCounter/ErrorCounter";
 
 export function GamePage() {
   const { pairsCount } = useParams();
 
+  const isActiveEasyMode = useSelector(state => state.game.isActiveEasyMode);
+
   return (
     <>
       <Cards pairsCount={parseInt(pairsCount, 10)} previewSeconds={5}></Cards>
+      {isActiveEasyMode && <ErrorCounter />}
     </>
   );
 }
