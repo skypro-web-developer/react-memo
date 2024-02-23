c9511581a8a39c5f8f04f210af6a2a29dacca968
duffblackraven <t9164700668@gmail.com>
2024-02-23 20:34:02 +0100

    Memo_game_hw1

--- src/context/ModeContext.jsx
+++ src/context/ModeContext.jsx
@@ -2,11 +2,15 @@ import { createContext, useState } from "react";
 
 const initialState = {
   isEnabled: false,
+  level: null,
+  setLevel: () => {},
   setIsEnabled: () => {},
 };
 
 export const ModeContext = createContext(initialState);
 export const ModeProvider = ({ children }) => {
   const [isEnabled, setIsEnabled] = useState(false);
-  return <ModeContext.Provider value={{ isEnabled, setIsEnabled }}>{children}</ModeContext.Provider>;
+  const [level, setLevel] = useState(null);
+
+  return <ModeContext.Provider value={{ isEnabled, setIsEnabled, level, setLevel }}>{children}</ModeContext.Provider>;
 };
