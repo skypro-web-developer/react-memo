2d885a842ffb50b70082be47f745ba5f1375289b
Viktor Pirogov <Blade-85@inbox.ru>
2024-02-26 14:02:00 +0300

    Подсоединение API

--- src/components/EndGameModal/EndGameModal.jsx
+++ src/components/EndGameModal/EndGameModal.jsx
@@ -5,9 +5,41 @@ import { Button } from "../Button/Button";
 import deadImageUrl from "./images/dead.png";
 import celebrationImageUrl from "./images/celebration.png";
 import { Link } from "react-router-dom";
+import { getLeaderBoard, postLeaderBoard } from "../../api";
+import { useContext, useEffect, useState } from "react";
+import { GameContext } from "../../Context";
 
 export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, onClick, isLeaderboard }) {
-  const title = isWon ? "Вы победили!" : "Вы проиграли!";
+  const { level } = useContext(GameContext);
+  const [leader, setLeader] = useState("Пользователь");
+  const [newLeader, setNewLeader] = useState(false);
+  const gameTime = gameDurationMinutes * 60 + gameDurationSeconds;
+  useEffect(() => {
+    if (level === "9" && isWon) {
+      getLeaderBoard().then(({ leaders }) => {
+        leaders = leaders.sort(function (a, b) {
+          return a.time - b.time;
+        });
+        if (leaders.length > 0 && leaders[0].time < gameTime) {
+          setNewLeader(true);
+        }
+      });
+    }
+  }, []);
+
+  function addPlayerToLeaders() {
+    postLeaderBoard({
+      name: leader,
+      time: gameTime,
+    })
+      .then(({ leaders }) => {
+        console.log(leaders);
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  }
+  const title = isWon ? "Вы попали на лидерборд!" : "Вы проиграли!";
 
   const imgSrc = isWon ? celebrationImageUrl : deadImageUrl;
 
@@ -18,13 +50,56 @@ export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, 
     <div className={styles.modal}>
       <img className={styles.image} src={imgSrc} alt={imgAlt} />
       <h2 className={styles.title}>{title}</h2>
+      {newLeader ? (
+        <input
+          className={styles.input_user}
+          type="text"
+          placeholder={"Пользователь "}
+          onChange={e => {
+            setLeader(e.target.value);
+          }}
+        />
+      ) : (
+        <div></div>
+      )}
       <p className={styles.description}>Затраченное время:</p>
       <div className={styles.time}>
         {gameDurationMinutes.toString().padStart("2", "0")}.{gameDurationSeconds.toString().padStart("2", "0")}
       </div>
-
-      <Button onClick={onClick}>Начать сначала</Button>
-      <Link className={styles.linkLidebord}>Перейти к лидерборду</Link>
+      {newLeader ? (
+        <>
+          <Button
+            onClick={() => {
+              addPlayerToLeaders();
+              onClick();
+            }}
+          >
+            Начать сначала
+          </Button>
+          <Link to="/">
+            <Button
+              onClick={() => {
+                addPlayerToLeaders();
+                onClick();
+              }}
+            >
+              На главную
+            </Button>
+          </Link>
+        </>
+      ) : (
+        <>
+          <Button onClick={onClick}>Начать сначала</Button>
+          <Link to="/">
+            <Button>На главную</Button>
+          </Link>
+        </>
+      )}
+      {newLeader && (
+        <Link to="/leaderboard" className={styles.linkBoard}>
+          Перейти к лидерборду
+        </Link>
+      )}
     </div>
   );
 }
--- src/pages/LeaderBoard/LeaderBoard.jsx
+++ src/pages/LeaderBoard/LeaderBoard.jsx
@@ -1,23 +1,70 @@
 import { useEffect, useState } from "react";
 import { getLeaderBoard } from "../../api";
 import { useGameContext } from "../../Context";
-
+import styles from "./LeaderBoard.module.css";
+import { Button } from "../../components/Button/Button";
+import { Link } from "react-router-dom";
 export function LeaderBoard() {
   const { leaderboardPlayers, setLeaderboardPlayers } = useGameContext();
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    getLeaderBoard().then(res => {
-      setLeaderboardPlayers(res);
-      setIsLoading(false);
-    });
+    getLeaderBoard()
+      .then(data => {
+        if (data.leaders) {
+          let leader = data.leaders;
+          leader = leader.sort(function (a, b) {
+            return a.time - b.time;
+          });
+          setLeaderboardPlayers(leader);
+          setIsLoading(false);
+        } else {
+          alert("Данные LeaderBoard не были получены.");
+        }
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
   }, []);
   if (isLoading) {
-    return "Загрузка";
+    return "Loading LeaderBoard... ";
   }
-  console.log(leaderboardPlayers);
+
   return (
-    <div>
-      <input placeholder="Чемпион"></input>
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <p className={styles.text}>Лидерборд</p>
+          <Link to="/">
+            <Button>Начать игру</Button>
+          </Link>
+        </div>
+        <table>
+          <thead className={styles.thead}>
+            <tr className={styles.leaderboard}>
+              <th className={styles.position}>Позиция</th>
+              <th className={styles.user}>Пользователь</th>
+              {/* <th className={styles.achievements}>Достижения</th> */}
+              <th className={styles.time}>Время</th>
+            </tr>
+          </thead>
+          <tbody className={styles.tbody}>
+            {leaderboardPlayers.map((leader, index) => (
+              <tr className={styles.leader} key={leader.id}>
+                <td className={styles.position}>#{index + 1}</td>
+                <td className={styles.user}>{leader.name}</td>
+                {/* <td className={styles.time}>{leader.time}</td> */}
+                <td className={styles.time}>
+                  {Math.floor(leader.time / 60)
+                    .toString()
+                    .padStart(2, "0")}
+                  :{(leader.time % 60).toString().padStart(2, "0")}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
