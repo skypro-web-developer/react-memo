110a43c141f384327e582097382890856531ed61
duffblackraven <t9164700668@gmail.com>
2024-02-29 15:25:58 +0100

    Fixed

--- src/components/EndGameModal/EndGameModal.jsx
+++ src/components/EndGameModal/EndGameModal.jsx
@@ -53,8 +53,8 @@ export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, 
 
   // const title = isWon ? "Вы попали на лидерборд!" : "Вы проиграли!";
 
-  // const title = isWon ? (level === "9" ? "Вы попали на лидерборд!" : "Вы победили!") : "Вы проиграли!";
-  const title = isWon ? (level === "3" ? "Вы попали на лидерборд!" : "Вы победили!") : "Вы проиграли!";
+  const title = isWon ? (level === "9" ? "Вы попали на лидерборд!" : "Вы победили!") : "Вы проиграли!";
+  // const title = isWon ? (level === "3" ? "Вы попали на лидерборд!" : "Вы победили!") : "Вы проиграли!";
 
   const imgSrc = isWon ? celebrationImageUrl : deadImageUrl;
 
